3a6066a250ef4f0b7a2596812e716ddb84c08045
Charles-Andre Bouffard <cabouffard@gmail.com>
2019-05-10 20:34:09 -0400

    Handle smooth scrolling when clicking nav buttons
    Required JQuery full, not the slim version, in order to use animate().

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,42 @@
 $(function () {
     activateCarousels();
     activateAlternateImage();
+    handleSmoothNavigationById();
+    changeNavBehavior();
 })
 
 
 $(window).scroll(function () {
     changeNavBehavior();
 })
+
+function handleSmoothNavigationById() {
+    // handle links with @href started with '#' only
+    $(document).on('click', 'a[href*="#"]', function(e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // Split string if string contains URL
+        if (id.charAt(0) !== '#') {
+            id = '#'.concat(id.split('#', 2)[1])
+        }
+
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document
+        var pos = $id.offset().top;
+
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos});
+    });
+}
 
 
 function activateCarousels() {
